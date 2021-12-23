fd7891eae5816a7bae81373457729d5d6c710211
kamal14rajput <kamal14rajput@gmail.com>
2021-12-24 01:08:14 +0530

    UPDATED: peak and pop from stack

--- src/com/bridgelabz/stack/StackMain.java
+++ src/com/bridgelabz/stack/StackMain.java
@@ -1,13 +1,16 @@
 package com.bridgelabz.stack;
 
-public class StackMain {
-
-	public static void main(String[] args) {
+public class StackMain 
+{
+	public static void main(String[] args) {		
 		StacksService<Integer> stack = new StacksService<Integer>();
 		stack.push(70);
 		stack.push(30);
 		stack.push(56);
 		stack.print();
+		
+		stack.peak();
+		stack.pop();
+		stack.print();
 	}
-
 }
