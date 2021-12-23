34093ffeee727591686463d2f0402bc6b689e3ec
kamal14rajput <kamal14rajput@gmail.com>
2021-12-24 01:02:46 +0530

    ADDED: sort linkedlist

---
+++ src/com/bridgelabz/stack/SortedLinkedList.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.stack;
+
+public class SortedLinkedList
+{
+	public int sortData;
+	public SortedLinkedList sortNext;
+	public SortedLinkedList()
+	{
+		super();
+		
+	}
+
+}
