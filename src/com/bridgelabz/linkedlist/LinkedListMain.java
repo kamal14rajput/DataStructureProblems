0dcb8e2f23c746d4de90e12c535acc702364bdbc
kamal14rajput <kamal14rajput@gmail.com>
2021-12-24 00:12:23 +0530

    UPDATED: delete first element

--- src/com/bridgelabz/linkedlist/LinkedListMain.java
+++ src/com/bridgelabz/linkedlist/LinkedListMain.java
@@ -2,27 +2,31 @@ package com.bridgelabz.linkedlist;
 
 public class LinkedListMain {
 
-	public static void main(String[] args) {
-		MyLinkedList<Integer> mylinkedlist = new MyLinkedList<Integer>();
-		
-		System.out.println("Using add method: ");
-		mylinkedlist.add(70);
-		mylinkedlist.add(30);
-		mylinkedlist.add(56);
-		
-		mylinkedlist.display();
-		
-		System.out.println("\nUsing append method");
-		mylinkedlist.append(56);
-		mylinkedlist.append(30);
-		mylinkedlist.append(70);
-		mylinkedlist.display();	
-		
-		System.out.println("\nAdd number in between");
-		mylinkedlist.append(56);
-		mylinkedlist.append(70);
-		mylinkedlist.insertAt(7, 30);
-		mylinkedlist.display();
-	}
+		public static void main(String[] args) {
+			MyLinkedList<Integer> mylinkedlist = new MyLinkedList<Integer>();
+			
+			System.out.println("Using add method: ");
+			mylinkedlist.add(70);
+			mylinkedlist.add(30);
+			mylinkedlist.add(56);
+			
+			mylinkedlist.display();
+			
+			System.out.println("\nUsing append method");
+			mylinkedlist.append(56);
+			mylinkedlist.append(30);
+			mylinkedlist.append(70);
+			mylinkedlist.display();	
+			
+			System.out.println("\nAdd number in between");
+			mylinkedlist.append(56);
+			mylinkedlist.append(70);
+			mylinkedlist.insertAt(1, 30);
+			mylinkedlist.display();
+			
+			System.out.println("\nDelete First element");
+			mylinkedlist.popFirst();
+			mylinkedlist.display();
+		}
 
-}
+	}
