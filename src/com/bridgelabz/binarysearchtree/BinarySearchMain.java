c276603771d73e68210a0b4cae458d637d288f02
kamal14rajput <kamal14rajput@gmail.com>
2021-12-27 01:59:23 +0530

    ADDED: binary search tree created

---
+++ src/com/bridgelabz/binarysearchtree/BinarySearchMain.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.binarysearchtree;
+
+public class BinarySearchMain {
+
+	public static void main(String[] args) {
+		BinarySearchService BST = new BinarySearchService();
+		BST.insert(56);
+		BST.insert(30);
+		BST.insert(70);
+	}
+
+}
